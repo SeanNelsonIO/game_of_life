0898621a7103b4e1f3c58442c827ea7cee02322d
Sean Nelson <ll17s2n@leeds.ac.uk>
2023-03-30 16:44:31 +0100

    futher implemented methods for cellular automata

---
+++ main.py
@@ -0,0 +1,5 @@
+from src import CellularAutomata
+from src import game_of_life_rule
+
+if __name__ == "__main__":
+    ca = CellularAutomata([10,10], 1, game_of_life_rule)
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from src.cellular_automata import CellularAutomata
+from src.rules import *
