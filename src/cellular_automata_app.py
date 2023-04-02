e899db53c05aa8ebddae2328e53f05139e90dd57
Steven Taylor <Steven@taylorcrew.us>
2023-04-02 23:19:49 +0100

    Themeing changes, added control panel for button control. Speed slider added however implementation needs changing

--- src/cellular_automata_app.py
+++ src/cellular_automata_app.py
@@ -5,6 +5,7 @@ import random
 
 from pygame_gui.elements import (
     UIButton,
+    UIHorizontalSlider,
     UILabel,
     UIPanel,
     UITextBox,
@@ -27,8 +28,20 @@ class CellularAutomataApp:
         self.window_surface = pygame.display.set_mode(self.window_size)
         self.ui_manager = UIManager(self.window_size, "data/themes/theme.json")
 
+        self.ui_manager.preload_fonts(
+            [{"name": "fira_code", "point_size": 14, "style": "bold"}]
+        )
+
         self.background = pygame.Surface(self.window_size)
         self.background.fill(self.ui_manager.ui_theme.get_colour("dark_bg"))
+
+        self.fps = 60
+
+        self.control_panel = None
+        self.spped_slider_label = None
+        self.speed_slider = None
+        self.pause_button = None
+        self.next_button = None
 
         self.seed_panel = None
         self.random_seed_button = None
@@ -38,11 +51,6 @@ class CellularAutomataApp:
 
         self.rules_panel = None
         self.rules_dropdown = None
-
-        self.control_panel = None
-        self.speed_slider = None
-        self.pause_button = None
-        self.next_button = None
 
         self.state_panel = None
         self.save_state_button = None
@@ -59,31 +67,82 @@ class CellularAutomataApp:
 
         # Setup Cellular Automata Grid with defaults
         default_rule = rules.game_of_life_rule
-        self.grid_padding = (296, 50)
+        self.grid_padding = (266, 50)
         self.cell_grid = CellGrid(default_rule, (100, 100))
 
-        panel_item_rect = pygame.Rect(10, 5, 175, 30)
+        default_panel_item_rect = pygame.Rect(10, 5, 175, 30)
 
-        self.create_seed_panel(panel_item_rect)
-        self.create_rules_panel(panel_item_rect)
-        self.create_control_panel(panel_item_rect)
-        self.create_state_panel(panel_item_rect)
+        self.create_control_panel(default_panel_item_rect)
+        self.create_seed_panel(default_panel_item_rect)
+        self.create_rules_panel(default_panel_item_rect)
+        self.create_state_panel(default_panel_item_rect)
 
-    def create_seed_panel(self, panel_item_rect: pygame.Rect) -> None:
-        self.seed_panel = UIPanel(
-            pygame.Rect(48, 48, 200, self.cell_grid.window_size[1] + 4),
+    def create_control_panel(self, panel_item_rect: pygame.Rect) -> None:
+        self.control_panel = UIPanel(
+            pygame.Rect(48, 48, 200, 126),
             starting_layer_height=4,
             manager=self.ui_manager,
         )
 
-        # Panel item rect defaults
-        pad_width = panel_item_rect.x
-        pad_height = panel_item_rect.y
-        height = panel_item_rect.height
-        width = panel_item_rect.width
+        self.spped_slider_label = UILabel(
+            panel_item_rect,
+            "Speed: 60 Iterations/s",
+            manager=self.ui_manager,
+            container=self.control_panel,
+        )
+
+        self.speed_slider = UIHorizontalSlider(
+            panel_item_rect,
+            60,
+            (1, 120),
+            manager=self.ui_manager,
+            container=self.control_panel,
+            anchors={"top_target": self.spped_slider_label},
+        )
+
+        self.pause_button = UIButton(
+            pygame.Rect(
+                panel_item_rect.x,
+                panel_item_rect.y * 2,
+                83,
+                panel_item_rect.height,
+            ),
+            "Play",
+            manager=self.ui_manager,
+            container=self.control_panel,
+            object_id="#pause_button",
+            anchors={"top_target": self.speed_slider},
+        )
+
+        self.next_button = UIButton(
+            pygame.Rect(
+                101,
+                panel_item_rect.y * 2,
+                83,
+                panel_item_rect.height,
+            ),
+            "Next",
+            manager=self.ui_manager,
+            container=self.control_panel,
+            object_id="#next_button",
+            anchors={"top_target": self.speed_slider},
+        )
+
+    def create_seed_panel(self, panel_item_rect: pygame.Rect) -> None:
+        self.seed_panel = UIPanel(
+            pygame.Rect(48, 16, 200, 196),
+            starting_layer_height=4,
+            manager=self.ui_manager,
+            anchors={"top_target": self.control_panel},
+        )
 
         self.random_seed_button = UIButton(
-            pygame.Rect(pad_width, pad_height * 2, width, height),
+            pygame.Rect(
+                panel_item_rect.x,
+                panel_item_rect.y * 2,
+                panel_item_rect.width,
+                panel_item_rect.height,
+            ),
             "Generate Seed",
             manager=self.ui_manager,
             container=self.seed_panel,
@@ -127,13 +186,13 @@ class CellularAutomataApp:
             anchors={"top_target": self.current_seed_button},
         )
 
-        # self.seed_text_box = UITextBox()
-
     def create_rules_panel(self, panel_item_rect: pygame.Rect) -> None:
-        pass
-
-    def create_control_panel(self, panel_item_rect: pygame.Rect) -> None:
-        pass
+        self.rules_panel = UIPanel(
+            pygame.Rect(48, 16, 200, 196),
+            starting_layer_height=4,
+            manager=self.ui_manager,
+            anchors={"top_target": self.seed_panel},
+        )
 
     def create_state_panel(self, panel_item_rect: pygame.Rect) -> None:
         pass
@@ -171,7 +230,16 @@ class CellularAutomataApp:
             self.set_current_seed_tooltip()
 
     def pause(self) -> None:
-        self.is_paused = not self.is_paused
+        if self.is_paused:
+            self.pause_button.set_text("Pause")
+            self.is_paused = False
+        else:
+            self.pause_button.set_text("Play")
+            self.is_paused = True
+
+    def next(self) -> None:
+        if self.is_paused:
+            self.cell_grid.update()
 
     def process_mouseclick(self, event) -> None:
         pos = pygame.mouse.get_pos()
@@ -180,10 +248,16 @@ class CellularAutomataApp:
     def process_keypress(self, event) -> None:
         if event.key == pygame.K_SPACE:
             self.pause()
-        if self.is_paused and event.key == pygame.K_RETURN:
-            self.cell_grid.update()
+        if event.key == pygame.K_RETURN:
+            self.next()
 
     def process_button_press(self, event):
+        if event.ui_element == self.pause_button:
+            self.pause()
+
+        if event.ui_element == self.next_button:
+            self.next()
+
         if event.ui_element == self.random_seed_button:
             new_seed = random.randint(0, 100000)
             self.seed_text_entry.set_text(str(new_seed))
@@ -221,12 +295,16 @@ class CellularAutomataApp:
             if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
                 self.process_text_entry(event)
 
+            if self.speed_slider.has_moved_recently:
+                self.fps = self.speed_slider.get_current_value()
+                self.spped_slider_label.set_text(f"Speed: {self.fps} Iterations/s")
+
         if not self.is_paused:
             self.cell_grid.update()
 
     def run(self) -> None:
         while self.is_running:
-            time_delta = self.clock.tick(60) / 1000.0
+            time_delta = self.clock.tick(self.fps) / 1000.0
 
             self.process_events()
 
