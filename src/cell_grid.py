64626acac7e802312e3ad127ea81e4b86f3eb3e1
Sean Nelson <ll17s2n@leeds.ac.uk>
2023-05-10 15:42:58 +0100

    new painter class that handles cell_grid painting(composition) fully working

--- src/cell_grid.py
+++ src/cell_grid.py
@@ -6,6 +6,8 @@ from src import colours
 from src.cellular_automata import CellularAutomata
 from math import cos, sin
 
+from src.painter import Painter
+
 
 class CellGrid:
     def __init__(self, rule: Callable, dimensions: tuple[int, int]) -> None:
@@ -32,6 +34,9 @@ class CellGrid:
         self.set_dimensions(dimensions)
         self.set_window_size()
         self.set_ca()
+
+        # Setup Painter object
+        self.set_painter()
 
         # Setup grid colours
         self.bg_colour = colours.BLACK
@@ -79,6 +84,9 @@ class CellGrid:
     def update(self) -> None:
         self.ca.update_grid()
 
+    def set_painter(self) -> None:
+        self.painter = Painter(self)
+
     def click(self, pos, padding) -> None:
         col = (pos[0] - padding[0]) // (self.cell_width + self.cell_margin)
         row = (pos[1] - padding[1]) // (self.cell_height + self.cell_margin)
@@ -90,93 +98,13 @@ class CellGrid:
         self.ca.grid[row][col] = not self.ca.grid[row][col]
 
         # self.draw_circle(pos, padding)
-
         print(f"Mouse down: {pos} at Grid: {row},{col}")
 
-    def draw_circle(self, pos, padding, brush_size, fill_value=1) -> None:
-        for angle in range(1, 360):
-            x1 = brush_size * cos(angle * 3.1415926535 / 180)
-            y1 = brush_size * sin(angle * 3.1415926535 / 180)
-
-            circumference_x = int(pos[0] + x1)
-            circumference_y = int(pos[1] + y1)
-
-            col = (circumference_x - padding[0]) // (self.cell_width + self.cell_margin)
-            row = (circumference_y - padding[1]) // (
-                self.cell_height + self.cell_margin
-            )
-
-            if not self.is_position_in_grid(row, col):
-                return
-
-            self.ca.grid[row][col] = fill_value
-
-            self.interpolate_cells(
-                pos, (circumference_x, circumference_y), padding, fill_value=fill_value
-            )
-
-    # TODO: refactor this so interpolation works correctly and more efficiently
     def paint(self, previous_pos, current_pos, padding, brush_size) -> None:
-        col = (current_pos[0] - padding[0]) // (self.cell_width + self.cell_margin)
-        row = (current_pos[1] - padding[1]) // (self.cell_height + self.cell_margin)
+        self.painter(previous_pos, current_pos, padding, brush_size)
 
-        if not self.is_position_in_grid(row, col):
-            return
-
-        # self.ca.grid[row][col] = 1
-        # print(f"Mouse down: {current_pos} at Grid: {row},{col}")
-        self.draw_circle(current_pos, padding, brush_size)
-
-        # interpolate between previous and current position
-        self.interpolate_cells(previous_pos, current_pos, padding)
-
-    # TODO: refactor this so interpolation works correctly and more efficiently
     def erase(self, previous_pos, current_pos, padding, brush_size) -> None:
-        col = (current_pos[0] - padding[0]) // (self.cell_width + self.cell_margin)
-        row = (current_pos[1] - padding[1]) // (self.cell_height + self.cell_margin)
-
-        if not self.is_position_in_grid(row, col):
-            return
-
-        # self.ca.grid[row][col] = 0
-        # print(f"Mouse down: {current_pos} at Grid: {row},{col}")
-
-        self.draw_circle(current_pos, padding, brush_size, fill_value=0)
-
-        # interpolate between previous and current position
-        self.interpolate_cells(previous_pos, current_pos, padding, fill_value=0)
-
-    # TODO: refactor this so interpolation works correctly and more efficiently
-    def interpolate_cells(
-        self, previous_pos, current_pos, padding, fill_value=1
-    ) -> None:
-        if previous_pos == current_pos:
-            return
-        if previous_pos is None:
-            return
-
-        dx = abs(current_pos[0] - previous_pos[0])
-        dy = abs(current_pos[1] - previous_pos[1])
-
-        if dx == 0 and dy == 0:
-            return
-
-        step_size = 1 / max(dx, dy)
-
-        t = 0.0
-        while t < 1:
-            x = int((1 - t) * previous_pos[0] + (t * current_pos[0]))
-            y = int((1 - t) * previous_pos[1] + (t * current_pos[1]))
-
-            col = (x - padding[0]) // (self.cell_width + self.cell_margin)
-            row = (y - padding[1]) // (self.cell_height + self.cell_margin)
-
-            if not self.is_position_in_grid(row, col):
-                return
-
-            self.ca.grid[row][col] = fill_value
-
-            t = t + step_size
+        self.painter(previous_pos, current_pos, padding, brush_size, erase=True)
 
     def is_position_in_grid(self, row, col) -> bool:
         row_in_bounds = self.height > row >= 0
